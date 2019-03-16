901aea5c44cb0ab8362d31c8db450628e001813c
Mano Sriram <mano.sriram0@gmail.com>
2019-03-16 23:31:26 +0530

    modified Index.js

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ mongoose
   .then(() => console.log("MongoDB Connected Succesfully!"))
   .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use("/auth", require("./routes/auth"));
 app.use("/api", require("./routes/api"));
 app.get("/", (req, res) => {
